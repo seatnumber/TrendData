cc4363184fc5cb65d0d8a02ce177ab9bf104a8bc
Frank Zhuo <526423681@qq.com>
2023-02-06 07:17:03 +0000

    添加accountDetail

--- router/router.ts
+++ router/router.ts
@@ -39,3 +39,40 @@ router.get('/account', async (ctx: any)=>{
          result: result
      }
 })
+
+router.get('/accountDetail', async (ctx: any)=>{
+    let result = await global.mongodb.collection('account').aggregate( [
+        // Stage 1: Filter pizza order documents by pizza size
+        {
+           $match: {createtime:{$gte:new Date(Date.now() - 3600000)}}
+        },
+        {
+            $sort: { createtime: -1 }
+        },
+        // Stage 2: Group remaining documents by pizza name and calculate total quantity
+        {
+           $group: { _id: "$servicename", profit:{$first: "$profit"}, bidPercent: {$first:"$bidPercent"}, askPercent: {$first:"$askPercent"},
+           positionValue:{$first:"$positionValue"},maxProfit:{$first:"$maxProfit"},maxDrawdown:{$first:"$maxDrawdown"},leverage:{$first:"$leverage"},createtime:{$first: "$createtime"},position: {$first: "$position"},
+           sumObject:{$first:"$sumObject"}, account: {$first: "$account"}}
+        },
+        {
+            $addFields: {
+                drawdown: { $subtract: ["$maxProfit","$profit"]}
+            }
+        },
+        {
+            $project: {
+                profit:"$profit",drawdown: "$drawdown",bidPercent:"$bidPercent",askPercent:"$askPercent",positionValue:"$positionValue",maxProfit:"$maxProfit",maxDrawdown:"$maxDrawdown",
+                leverage:"$leverage",createtime:"$createtime",position:"$position",sumObject:"$sumObject",account:"$account"
+            }
+        },
+        {
+            $sort: { profit: -1}
+        }
+     ] ).toArray()
+     ctx.response.type = 'json'
+     ctx.body = {
+         code: 10000,
+         result: result
+     }
+})
