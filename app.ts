a9db46f5014c639b0295a5a6b7ca900cea0bd1ad
Frank Zhuo <526423681@qq.com>
2023-03-16 14:40:18 +0000

    fix

--- app.ts
+++ app.ts
@@ -1,35 +1,30 @@
-import { start } from "repl";
+import Koa from 'koa';
+import serve from 'koa-static';
+import path from 'path';
+import fs from 'fs';
+import { config } from './config/config';
+import { router } from './router/router';
+import * as mongoconnect from './middleware/connectMongo';
 
-const Koa = require('koa');
-const serve = require('koa-static');
 const app = new Koa();
-const fs = require('fs');
-const path = require('path');
-
 const accountListHTML = fs.readFileSync(path.join(__dirname, 'public', 'account_list.html'), 'utf8');
 
-import { router } from './router/router'
-import { config } from './config/config'
-
-const mongoconnect = require('./middleware/connectMongo.js')
-
-
 async function startUp() {
-  global.mongodb = await mongoconnect.connect(config.mongohost, config.database)
-  // response
+  global.mongodb = await mongoconnect.connect(config.mongohost, config.database);
+
   // Serve static files
   app.use(serve('public'));
 
   // Default route
   router.get('/', async (ctx: any) => {
     ctx.type = 'text/html';
-    console.log('IP: ',process.env.SERVER_IP)
     ctx.body = accountListHTML.replace('SERVER_IP', process.env.SERVER_IP || '35.74.231.172');
   });
+
   app.use(router.routes());
   app.use(router.allowedMethods());
 
   app.listen(8081);
 }
 
-startUp()
+startUp();
