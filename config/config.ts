586b482456e62e7147948ffeb734d5cce0b8f5a1
Frank Zhuo <526423681@qq.com>
2023-07-30 07:42:53 +0000

    v2

--- config/config.ts
+++ config/config.ts
@@ -1,6 +1,6 @@
 export const config: Config = {
     processPath : __dirname+'/../',
-    version: 1.27,
+    version: 2,
     mongohost:process.env.mongohost ? process.env.mongohost : '',
     database:process.env.database ? process.env.database : ''
 }
